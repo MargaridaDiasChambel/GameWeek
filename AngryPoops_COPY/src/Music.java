d68215b77000bdb72803274105e2004a88db1e32
Margarida Chambel <margarida.dias.chambel@gmail.com>
2022-12-20 18:35:03 +0000

    first commit

---
+++ AngryPoops_COPY/src/Music.java
@@ -0,0 +1,42 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Music {
+
+
+    public void startMusic(String pathStr) {
+        URL soundURL;
+        AudioInputStream audio = null;
+
+        try {
+            soundURL = Main.class.getResource(pathStr);
+            if(soundURL == null) {
+                pathStr = pathStr.substring(1);
+                File file = new File(pathStr);
+                soundURL = file.toURI().toURL();
+            }
+            audio = AudioSystem.getAudioInputStream(soundURL);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.start();
+            clip.loop(clip.LOOP_CONTINUOUSLY);
+
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
